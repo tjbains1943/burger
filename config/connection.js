48d68d870597cc017cd4b4c02f58eb42635705f1
tj Bains <tjbains1943@yahoo.com>
2018-05-03 00:08:40 -0700

    add space in mysql

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 var mysql = require("mysql");
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'root',
-  password : 'root',
+  user     : 'root' ,
+  password : 'root' ,
   database : 'burgers_db'
 });
  
