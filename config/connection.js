0aed5cc98a0181b19f843ff25d84fdc5ab4b1cf8
tj Bains <tjbains1943@yahoo.com>
2018-05-03 00:46:36 -0700

    remove port

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,6 @@
 var mysql = require("mysql");
 var connection = mysql.createConnection({
   host     : 'localhost',
-  port: "3306" ,
   user     : 'root' ,
   password : 'root' ,
   database : 'burgers_db'
