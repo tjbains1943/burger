713aa98edd572c10993131ae47d0b112bf3f2222
tj Bains <tjbains1943@yahoo.com>
2018-05-03 00:34:21 -0700

    add port

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var connection = mysql.createConnection({
   host     : 'localhost',
+  port: "3306",
   user     : 'root' ,
   password : 'root' ,
   database : 'burgers_db'
